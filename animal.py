5c1ab7c2fc10f8e93a68c88ce1a9d570838f7ac8
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-08-25 16:35:25 -0600

    Adding Animal class

---
+++ animal.py
@@ -0,0 +1,26 @@
+class Animal:
+    name: str
+    
+    def __init__(self, name: str):
+        self.name = name
+    
+    def speak(self):
+        return "Hace un sonido"
+    pass
+
+class Dog(Animal):
+    def speak(self):
+        return "Guau"
+
+class Cat(Animal):
+    def speak(self):
+        return "Miau"
+    
+dog_name = input("Introduzca un nombre para perro: ")
+cat_name = input("Introduzca un nombre para un gato: ")
+
+dog = Dog(dog_name)
+cat = Cat(cat_name)
+
+print(dog.speak())
+print(cat.speak())
