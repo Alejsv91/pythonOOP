20793ba85236c8a8657d0aaca49994c67aac7dd1
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-08-25 12:07:23 -0600

    Adding the Human class

--- body.py
+++ body.py
@@ -5,17 +5,11 @@ class Torso:
     head: Head
     left_arm: Arm
     right_arm: Arm
-    left_leg: Leg
-    right_let: Leg
     
-    def __init__(self, head: Head, left_arm: Arm, right_arm: Arm, left_leg: Leg, right_leg: Leg):
+    def __init__(self, head: Head, left_arm: Arm, right_arm: Arm):
         self.head = head
         self.left_arm = left_arm
         self.right_arm = right_arm
-        self.left_leg = left_leg
-        self.right_let = right_leg
-    
-    pass
 
 class Arm:
     def __init__(self, hand: Hand):
@@ -27,10 +21,19 @@ class Hand:
 class Leg:
     def __init__(self, feet: Feet):
         self.feet = feet
-    pass
 
 class Feet:
     pass
+
+class Human:
+    torso: Torso
+    left_leg: Leg
+    right_leg: Leg
+    
+    def __init__(self, torso: Torso, left_leg: Leg, right_leg: Leg):
+        self.torso = torso
+        self.left_leg = left_leg
+        self.right_leg = right_leg
 
 head = Head()
 right_hand = Hand()
@@ -41,8 +44,8 @@ right_feet = Feet()
 left_feet = Feet()
 right_leg = Leg(right_feet)
 left_leg = Leg(left_feet)
-
-torso = Torso(head, left_arm,right_arm, left_leg, right_leg)
+torso = Torso(head, left_arm,right_arm)
+human = Human(torso, left_leg, right_leg)
 
 
 
