01ea2c4a5532f7dea585a12f26a69ae0ec4b2968
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-09-14 11:18:54 -0600

    Adding decorator

--- user.py
+++ user.py
@@ -2,8 +2,9 @@ from datetime import date
 
 
 class User:
-    def __init__(self, date_of_birth: date):
+    def __init__(self, date_of_birth: date, drive_license: bool):
         self.date_of_birth = date_of_birth
+        self.drive_license = drive_license
 
     @property
     def age(self) -> int:
@@ -20,4 +21,21 @@ class User:
     
 def is_adult(func):
     def wrapper(user: User):
+        if user.age < 18:
+            raise ValueError(" User is not an adult")
+        else:
+            print("User is adult")
+        return func(user)
+    return wrapper
+            
+@is_adult
+def user_can_drive(user: User):
+    if user.drive_license:
+        print("User can drive")
+    else: 
+        print("User can't drive")
+    
         
+birthdate = date(2016,1,9)
+user = User(birthdate, True)
+user_can_drive(user)
