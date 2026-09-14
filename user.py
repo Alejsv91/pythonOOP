c67d24eeda4c8d1bee88a8d76a89acfa2a908d60
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-09-14 10:36:49 -0600

    Adding user changes

---
+++ user.py
@@ -0,0 +1,23 @@
+from datetime import date
+
+
+class User:
+    def __init__(self, date_of_birth: date):
+        self.date_of_birth = date_of_birth
+
+    @property
+    def age(self) -> int:
+        today = date.today()
+
+        return (
+            today.year
+            - self.date_of_birth.year
+            - (
+                (today.month, today.day)
+                < (self.date_of_birth.month, self.date_of_birth.day)
+            )
+        )
+    
+def is_adult(func):
+    def wrapper(user: User):
+        
