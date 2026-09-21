26391d0e4d9e0636e09fb40f3c86a251bfba9b08
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-09-21 15:04:05 -0600

    Adding outer index

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,6 @@
 def standard_bubble_sort(my_list: list[int]):
     for outer_index in range(0, len(my_list) -1):
-        for index in range(0, len(my_list) -1):
+        for index in range(0, len(my_list) -1 - outer_index):
             current_value = my_list[index]
             next_value = my_list[index + 1]
             if(current_value > next_value):
