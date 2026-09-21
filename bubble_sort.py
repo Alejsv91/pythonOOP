f6f3a7d625f1ce690c92375c1e08e38679130370
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-09-21 14:51:17 -0600

    Adding required changes

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,20 +1,26 @@
+def standard_bubble_sort(my_list: list[int]):
+    for outer_index in range(0, len(my_list) -1):
+        for index in range(0, len(my_list) -1):
+            current_value = my_list[index]
+            next_value = my_list[index + 1]
+            if(current_value > next_value):
+                my_list[index + 1] = current_value
+                my_list[index ]= next_value
+    print("--- standard Final list ---")
+    print(my_list)
+
 def bubble_sort(my_list: list[int]):
     for outer_index in range(len(my_list) -1):
-        print(f'iteration index: {outer_index}')
         for index in range(len(my_list) -1 , outer_index, -1):
             current_value = my_list[index]
             next_value = my_list[index - 1]
             if(current_value < next_value):
-                print(f"{current_value} is less than {next_value}")
                 my_list[index - 1] = current_value
                 my_list[index ]= next_value
-                print(f"moving {my_list[index - 1]} to index {index - 1} and {my_list[index]} to index: {index}")
-                print(my_list)
-            else:
-                print(f"{current_value} is higher than {next_value}")
-            print(f"---- index is {index} and value is {my_list[index]} ----")
     print("--- Final list ---")
     print(my_list)
             
 my_list =  [14, 6, 9, 10, 5 ,2 ,1, 15, -1]
-bubble_sort(my_list) 
+standard_bubble_sort(my_list) 
+
+bubble_sort(my_list)
