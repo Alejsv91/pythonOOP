fab24a95f82b3816b5c9c08544414780d3c8e23b
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-09-22 12:47:19 -0600

    Adding required exercises

--- functions.py
+++ functions.py
@@ -33,7 +33,25 @@ def count_upper_cases(value: str):
 
 # 6
 def order_string(value: str):
-    pass
+    split_value = value.split("-")
+    split_value.sort()
+    return split_value
+
+# 7
+def prime_numbers(numbers: list[int]) -> bool:
+    prime_list: list[int] = []
+    for n in numbers:
+        if n <= 1:
+            continue
+        
+        for divisor in range(2,int(n ** 0.5) + 1):
+            if n% divisor == 0:
+                break
+        else:
+            prime_list.append(n)
+    
+    return prime_list
+            
     
 
 
@@ -41,4 +59,10 @@ def order_string(value: str):
 # first_function()
 # print(global_variable)
 # print(reverse_string("First test"))
-print(f"Total upper: {count_upper_cases("This Is My Value")}")
+# print(f"Total upper: {count_upper_cases("This Is My Value")}")
+
+# value = "this-is-my-test"
+# print(f"Ordered{order_string(value)}")
+
+number_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+print(f"The prime numbers are: {prime_numbers(number_list)}")
