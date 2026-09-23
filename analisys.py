56bc11cda55216044300a35f856d17b2ef65b341
Alejsv91 <Alejandro91_cr@hotmail.com>
2026-09-23 12:51:07 -0600

    Adding bubble sort

--- analisys.py
+++ analisys.py
@@ -1,3 +1,25 @@
+def bubble_sort(my_list: list[int]): # O(n^2)
+    if not isinstance(my_list, list):
+        raise TypeError("This sort only works with a list")
+    
+    for outer_index in range(len(my_list) -1):
+        print(f'iteration index: {outer_index}')
+        for index in range(len(my_list) -1 , outer_index, -1):
+            current_value = my_list[index]
+            next_value = my_list[index - 1]
+            if(current_value < next_value):
+                print(f"{current_value} is less than {next_value}")
+                my_list[index - 1] = current_value
+                my_list[index ]= next_value
+                print(f"moving {my_list[index - 1]} to index {index - 1} and {my_list[index]} to index: {index}")
+                print(my_list)
+            else:
+                print(f"{current_value} is higher than {next_value}")
+            print(f"---- index is {index} and value is {my_list[index]} ----")
+    print("--- Final list ---")
+    print(my_list)
+    return my_list
+
 def print_numbers_times_2(numbers_list): #o(n)
 	for number in numbers_list:
 		print(number * 2)
